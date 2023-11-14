2d9b93d3f55a973381e85089ddda3a0985d5c222
Rudi Abrahamsen <rudi.abrah@gmail.com>
2023-11-14 15:11:47 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import React from "react";
-import { Typography } from '@material-ui/core';
+import { Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { PhotoCamera } from "@material-ui/icons";
+
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 const App = () => {
-    return( <div>
-        <Typography variant = "h1">Hello World!</Typography>
-        </div>
-        );
+    return(
+    <>
+    
+        <CssBaseline />
+        <AppBar position="relative">
+            <Toolbar>
+                <PhotoCamera />
+                <Typography variant="h6">
+                    Photo Album
+                </Typography>
+            </Toolbar>
+        </AppBar> 
+        <main>
+            <div>
+                <Container maxWidth="sm">
+                    <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                        Photo Album
+                    </Typography>
+                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                        Hello everyone this is a photo album. Github copilot is scary good.
+                    </Typography>
+                </Container>
+            </div>
+            <ButtonGroup variant="outlined" aria-label="outlined button group">
+                <Button>One</Button>
+                <Button>Two</Button>
+                <Button>Three</Button>
+            </ButtonGroup>
+        </main>
+    </>
+    );
 
 }
 
