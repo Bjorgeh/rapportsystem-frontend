27704d8aaaf766371b6d4fd4a1e1c425dfc50466
Rudi Abrahamsen <rudi.abrah@gmail.com>
2023-11-16 08:05:21 +0100

    Fixed
    La inn Anzure sin push til main her for å merge.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+<<<<<<< HEAD
+>>>>>>> parent of 2f08b6e (Merge pull request #13 from Bjorgeh/Clean_slate)
+=======
+>>>>>>> parent of 2f08b6e (Merge pull request #13 from Bjorgeh/Clean_slate)
