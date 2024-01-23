073f17a710e893a7f5397d008014c259c5e11d35
André Mathisen <andre.mathisen@odit.no>
2024-01-23 17:15:43 +0100

    Reverted latest template changes

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';//Import the App component from App.jsx
-
-
-//Rende the App component into the root element
-ReactDOM.render(<App />, document.getElementById('root'));
